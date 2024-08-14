82943271da7f19c875c01c9ca54f3842b323fc7a
seancoffman <coffmansean0314@gmail.com>
2024-08-14 11:53:41 -0700

    Add files via upload
    Uploaded my first C++ program

---
+++ helloWorld.cpp
@@ -0,0 +1,15 @@
+// sC 8/14/24
+// Sean Coffman
+// helloWorld.cpp
+/*
+ * this is my first C++ program
+ * it only took me an hour to write!
+ *
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\nHello, World! " << "  another string" << endl;
+    return 0;
+}
